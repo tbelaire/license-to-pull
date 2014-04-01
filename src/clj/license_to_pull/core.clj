0b07986cc35ace03ae797b563724e08dc2fd71b9
Theo Belaire <theobelaire@khanacademy.org>
2014-03-31 23:15:03 -0700

    Yay routes and factoring stuff.

--- src/clj/license_to_pull/core.clj
+++ src/clj/license_to_pull/core.clj
@@ -1,12 +1,12 @@
 (ns license-to-pull.core
-    (:require [compojure.handler :as handler]
-              [compojure.route :as route]
-              [compojure.core :refer [GET POST ANY defroutes]]
-              [ring.util.response :as resp]
-              [cheshire.core :as json]
-              [clojure.contrib.core :refer [-?>]]
-              [clojure.java.io :as io]
-              [tentacles.repos :as tent]))
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [compojure.core :refer [GET POST ANY defroutes routes context]]
+            [ring.util.response :as resp]
+            [cheshire.core :as json]
+            [clojure.contrib.core :refer [-?>]]
+            [clojure.java.io :as io]
+            [tentacles.repos :as tent]))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
@@ -17,28 +17,28 @@
   (if (re-matches (re-pattern "-?\\d+") str)
     (read-string str)))
 
-(defroutes app-routes
-  (GET "/" [] (resp/redirect "/index.html"))
-
+(defroutes api-routes
   (GET "/test" [] (json-response
-                   {:message "You made it!"}))
+                    {:message "You are testing number: "}))
 
   (POST "/test" req (json-response
-                     {:message "Doing something something important..."}))
+                      {:message "Doing something something important..."}))
 
   (ANY "/add" [num] (json-response
-                    (let [n (str->int num)]
-                      (if (nil? n)
-                        "NOPE"
-                        (+ 1 n)))))
+                      (let [n (str->int num)]
+                        (if (nil? n)
+                          "NOPE"
+                          (+ 1 n)))))
 
   (GET "/lookup/:userid/" [userid]
-       (json-response (tent/user-repos userid)))
+       (json-response (tent/user-repos userid))))
 
+(defroutes site-routes
+  (GET "/" [] (resp/redirect "/index.html"))
 
   (route/resources "/")
   (route/not-found "Page not found"))
 
 (def app
-  (-> #'app-routes
-      (handler/api)))
+  (routes (handler/api (context "/api" [] api-routes))
+          (handler/site site-routes)))
