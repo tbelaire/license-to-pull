f3dd12acbbb12770a06ca02e6e6e1eb5e841d1ff
Theo Belaire <theobelaire@khanacademy.org>
2014-06-07 15:58:37 -0400

    Dropped the cljs stuff, added react
    I'm just using the in browser transform for now.
    Later, I might add a task to compile the jsx offline ahead of time.
    Also less is gone, but that can come back later.
    I just want to keep it simple for now.

--- project.clj
+++ project.clj
@@ -11,18 +11,12 @@
                  [cheshire "5.2.0"]
                  [base64-clj "0.1.1"]
                  [clj-http "0.9.2"]
-                 ;; CLJS
-                 [org.clojure/clojurescript "0.0-2138"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [cljs-http "0.1.3"]
-                 [om "0.1.7"]
                  ;; Other
                  [tentacles "0.2.5"]
                  ;;; For something like secrets.py, but with env vars
                  [environ "0.5.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]
-            [lein-ring "0.8.7"]
+  :plugins [[lein-ring "0.8.7"]
             [lein-pdo "0.1.1"]
             [lein-lesscss "1.2"]
             [lein-environ "0.5.0"]]
@@ -30,31 +24,9 @@
   :lesscss-paths ["less"]
   :lesscss-output-path "resources/public/css"
 
-  :aliases {"dev" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
+  :aliases {"dev" ["pdo" "auto" "dev," "ring" "server-headless"]}
 
   :ring {:handler license-to-pull.core/app
          :init    license-to-pull.core/init}
 
-  :source-paths ["src/clj" "test/clj"]
-
-  :cljsbuild {
-              :builds [{:id "dev"
-                        :source-paths ["src/cljs"]
-                        :compiler {
-                                   :output-to "resources/public/js/license_to_pull.js"
-                                   :output-dir "resources/public/js/out"
-                                   :optimizations :none
-                                   :source-map true
-                                   :externs ["react/externs/react.js"]}}
-                       {:id "release"
-                        :source-paths ["src/cljs"]
-                        :compiler {
-                                   :output-to "resources/public/js/license_to_pull.js"
-                                   :source-map "resources/public/js/license_to_pull.js.map"
-                                   :optimizations :advanced
-                                   :pretty-print false
-                                   :output-wrapper false
-                                   :preamble ["react/react.min.js"]
-                                   :externs ["react/externs/react.js"]
-                                   :closure-warnings
-                                   {:non-standard-jsdoc :off}}}]})
+  :source-paths ["src/clj" "test/clj"])
